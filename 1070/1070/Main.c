b46cdc457ed05b7539a49d7c35c268de5f1f8d07
GiYoon_Kim <flgofk78@gmail.com>
2021-08-04 21:24:08 +0900

    1070 Problem solving

---
+++ 1070/1070/Main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int month;
+
+	scanf_s("%d", &month);
+
+	switch (month)
+	{
+	case 12:
+	case 1:
+	case 2:
+		puts("winter");
+		break;
+	case 3:
+	case 4:
+	case 5:
+		puts("spring");
+		break;
+	case 6:
+	case 7:
+	case 8:
+		puts("summer");
+		break;
+	case 9:
+	case 10:
+	case 11:
+		puts("fall");
+		break;
+	}
+
+	return 0;
+}
